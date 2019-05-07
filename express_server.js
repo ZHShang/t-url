f2c3b1e20098747dd3a614a05bd5191f91271dc8
ZHShang <zhihengshang@hotmail.com>
2019-05-07 18:49:40 +0000

    Took user input and placed it in the longURL key

--- express_server.js
+++ express_server.js
@@ -1,17 +1,33 @@
+const bodyParser = require("body-parser");
+
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {
@@ -30,6 +46,7 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 
 
 app.listen(PORT, () => {
