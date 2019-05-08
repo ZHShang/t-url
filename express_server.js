5ee9d45dc96e5d241c41125c9dc4ea03000da0e9
ZHShang <zhihengshang@hotmail.com>
2019-05-08 19:26:51 +0000

    deleted the excess tester codes

--- express_server.js
+++ express_server.js
@@ -74,18 +74,6 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 
 
 app.listen(PORT, () => {
